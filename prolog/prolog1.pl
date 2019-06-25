2e338217a5ad566d72534d0d5ee76791bc0ed095
Wade Huber <wadehuber@gmail.com>
2019-06-25 00:29:47 -0400

    Prolog 1 changes

--- prolog/prolog1.pl
+++ prolog/prolog1.pl
@@ -16,6 +16,8 @@ male(john).
 male(bob).
 male(mike).
 female(sarah).
+
+% weather/3
 weather(tempe, summer, hot).
 weather(tempe, winter, warm).
 weather(tempe, fall, hot).
@@ -23,10 +25,13 @@ weather(newyork, summer, warm).
 weather(seattle, summer, cool).
 weather(buenasaires, winter, warm).
 weather(sydney, fall, hot).
+
+% weather/2
 weather(City, Temp) :- weather(City, _, Temp).
+
+% warmcity/1
 warmcity(City) :- weather(City, _, warm).
 warmcity(City) :- weather(City, fall, hot).
-
 
 avg_temp(berlin, 49).
 avg_temp(karlsruhe, 60).
@@ -38,23 +43,23 @@ avg_temp(johannesburg, 55).
 avg_temp(phoenix, 80).
 avg_temp(jerusalem, 61).
 
-% calculate averate temperatur in Celsius
-avg_temp_celsius(City, C_temp) :-   % head of the rule
-		avg_temp(City, F_temp),     % get the avg temp of the City
-		C_temp is round((F_temp - 32) * 5 / 9).
+% avg_temp_celsius/2 - calculate the average temperature in Celsius
+avg_temp_celsius(City, C_temp) :-        % head of the rule
+				avg_temp(City, F_temp),  % get the avg temp of the City
+				C_temp is round((F_temp - 32) * 5 / 9).
 
-play_outside(City) :- avg_temp_celsius(City, Temp), Temp < 20.
+play_outside(City) :- avg_temp_celsius(City, Temp), Temp < 15.
 
 colder_than(City1, City2) :- 
-		avg_temp_celsius(City1, Temp1),
-		avg_temp_celsius(City2, Temp2),
-		Temp1 < Temp2.
+				avg_temp(City1, Temp1), 
+				avg_temp(City2, Temp2), 
+				Temp1 < Temp2.
 
 % To check equality, use the same variable name
-same_temp(City1, City2) :-
-		avg_temp_celsius(City1, Temp),
-		avg_temp_celsius(City2, Temp),
-		City1 \= City2.
+same_temp(City1, City2) :- 
+	avg_temp(City1, Temp),
+	avg_temp(City2, Temp),
+	City1 \= City2.
 
 president(washington).
 president(lincoln).
@@ -62,13 +67,20 @@ president(kennedy).
 president(obama).
 president(X) :- member(X,[adams, jefferson, fdr]).
 
+% not a good rule
+score(mets, 7).
+score(phillies, 13).
+score(patriots, 34).
+score(falcons, 28).
 
-% Not a good rule
-score(suns, 102).
-score(grizzlies, 100).
-won(X) :- score(X, ScoreX), score(_, ScoreY), ScoreX > ScoreY.
+won(X) :- score(X, ScoreX), score(_Y, ScoreY), ScoreX > ScoreY.
 
-% how can we improve the above?
+
+% better rule
+game(mets, 7, phillies, 13).
+game(patriots, 34, falcons, 28).
+won2(X) :- game(X, ScoreX, _Y, ScoreY), ScoreX > ScoreY.
+won2(Y) :- game(_X, ScoreX, Y, ScoreY), ScoreY > ScoreX.
 
 happy(bob).
 happy(alice).
@@ -86,19 +98,22 @@ badguy(batman, joker).
 badguy(superman, lexluthor).
 fight(X) :- badguy(X).
 fight(X) :- badguy(_, X).
-% poor bob & alice
+
+% Poor bob!
 fight(X) :- annoying(X).
+
+% Poor alice!
 fight(alice).
-fight(_).       % Nooooooooo!
+%fight(_).      % Noooooooooo!
 
 % write output when a file is consulted
 :- badguy(batman, Who), write('Batman has defeated '), write(Who), nl.
 
-same(X,X).
+
+is_integer(X) :- member(X, [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]).
 
 % equation A + B = C
-equation(A,B,C) :-
-	same(Integer,[0,1,2,3,4,5,6,7,8,10,11,12,13,14,15,16,17,18,19,20]),
-	member(A, Integer),
-	member(B, Integer), 
+equation(A, B, C) :-
+	is_integer(A),
+	is_integer(B),
 	C is A + B.
